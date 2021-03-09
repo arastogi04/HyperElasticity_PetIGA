6b2a77d3a984ee9ad09bc49805671c964d17d8b4
Animesh Rastogi <animesh.rastogi@alumni.iitgn.ac.in>
2021-03-09 09:52:25 -0600

    Code changed to 2D - solution not correct

--- run_code.sh
+++ run_code.sh
@@ -2,6 +2,6 @@
 
 make HyperElasticity
 
-./HyperElasticity -neohook -snes_max_it 50 -snes_atol 1e-10 -snes_type newtonls -snes_monitor -snes_converged_reason -iga_view
+./HyperElasticity -snes_max_it 50 -snes_atol 1e-10 -snes_type newtonls -snes_monitor -snes_converged_reason -iga_view
 
 python create_vtk.py
